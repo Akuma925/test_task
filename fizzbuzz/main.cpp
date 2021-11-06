86161561a37e34d64c8e27a3800fd91b05b927c1
Alex <madsilver925@gmail.com>
2021-11-07 02:08:00 +0300

    up

--- fizzbuzz/main.cpp
+++ fizzbuzz/main.cpp
@@ -3,13 +3,17 @@ using namespace std;
 
 int main ()
 {
-    for(int i=0; i<101; ++i)
-    {
-        if(i%3==0) cout<<"Fizz"<<endl;
-        if(i%5==0) cout<<"Buzz"<<endl;
-        if(i%3!=0 && i%5!=0)
-            cout<<"\n";
-        cout<<endl;
-
+    for(int i = 0; i < 101; ++i)
+    {      
+        string n = to_string(i), f = "Fizz", b="Buzz"; 
+        if(i % 3 != 0)
+            f = ""; 
+        else 
+            n = ""; 
+        if(i % 5 != 0)
+            b = "";
+        else 
+            n=""; 
+        cout<<n + f + b + "\n"; 
     }
 }
